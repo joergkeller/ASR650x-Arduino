e6b27d87a1bb2c74be16e0f5cb68db525db5d482
jokel <joerg.p.keller@gmail.com>
2019-11-18 17:30:09 +0100

    Issue 24: Missing microsecondsToClockCycles() macro (DHT workaround)

--- cores/asr650x/Arduino.h
+++ cores/asr650x/Arduino.h
@@ -64,6 +64,9 @@
 #define noInterrupts() CyGlobalIntDisable
 #define interrupts()   CyGlobalIntEnable
 
+#define clockCyclesPerMicrosecond() ( F_CPU / 1000000L )
+#define clockCyclesToMicroseconds(a) ( (a) / clockCyclesPerMicrosecond() )
+#define microsecondsToClockCycles(a) ( (a) * clockCyclesPerMicrosecond() )
 
 
 
